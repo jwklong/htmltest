3013c3121db9d112c989019a59652178b9440145
jwklong <jwklong@gmx.com>
2020-10-10 15:26:45 +0100

    v1.3.0-test3
    b o r d e r s

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # htmltest
 <h2>v1.2.2</h2>
 <h3>cool its a update log n stuff</h3>
-<h2>v1.3.0-test2</h2>
-<h3>as you see, i am still doing stuff for the JS despite me having to do alot of code. also JS icon :D</h3>
+<h2>v1.3.0-test3</h2>
+<h3>its obvious what im doing with the JS test, im gonna make it like those basic button simulators on roblox, also images now have borders so ye and new theme for the update log</h3>
 <a href="jwklong.github.io/htmltest"><h6><i>jwklong.github.io/htmltest</i></h6></a>
--- js.js
+++ js.js
@@ -1,8 +1,20 @@
 // this is the main js file for js.html, if you want to add more to the JS test edit the js here.
 
-counter = 0;
+let counter = 0;
+let multiplier = 0;
 
-function OnClick() {
-    counter = counter + 1;
-    document.getElementById("clickCount").innerHTML = counter;
+function OnClickMoney(amount, requires) {
+    if (requires <= multiplier) {
+        counter = counter + amount * (multiplier + 1);
+        document.getElementById("clickCount").innerHTML = counter;
+    }
+}
+
+function OnClickMultiplier(amount, cost) {
+    if (cost <= counter) {
+        multiplier = multiplier + amount;
+        counter = counter - cost
+        document.getElementById("multiplierCount").innerHTML = multiplier;
+        document.getElementById("clickCount").innerHTML = counter;
+    }
 }
