2fc0638181719bcb04689689a3bfa8f1cea7f8dc
jwklong <jwklong@gmx.com>
2020-10-11 09:43:05 +0100

    v1.3.0-rc1
     release candidate time

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # htmltest
 <h2>v1.2.2</h2>
 <h3>cool its a update log n stuff</h3>
-<h2>v1.3.0-test3</h2>
-<h3>its obvious what im doing with the JS test, im gonna make it like those basic button simulators on roblox, also images now have borders so ye and new theme for the update log</h3>
+<h2>v1.3.0-rc1</h2>
+<h3>what time is it? oh its release candidate time, rebirths gonna come soon, SPOON ICON, added apostrophes and yeah, expect the full version to come next</h3>
 <a href="jwklong.github.io/htmltest"><h6><i>jwklong.github.io/htmltest</i></h6></a>
--- js.js
+++ js.js
@@ -1,20 +1,32 @@
 // this is the main js file for js.html, if you want to add more to the JS test edit the js here.
 
-let counter = 0;
+let money = 0;
 let multiplier = 0;
+let rebirth = 0;
 
 function OnClickMoney(amount, requires) {
     if (requires <= multiplier) {
-        counter = counter + amount * (multiplier + 1);
-        document.getElementById("clickCount").innerHTML = counter;
+        money = money + amount * (multiplier + 1);
+        document.getElementById("moneyCount").innerHTML = money;
     }
 }
 
-function OnClickMultiplier(amount, cost) {
-    if (cost <= counter) {
-        multiplier = multiplier + amount;
-        counter = counter - cost
+function OnClickMultiplier(amount, cost, requires) {
+    if (requires <= rebirth) {
+        if (cost <= money) {
+            multiplier = multiplier + amount * (rebirth + 1);
+            money = money - cost
+            document.getElementById("multiplierCount").innerHTML = multiplier;
+            document.getElementById("moneyCount").innerHTML = money;
+        }
+    }
+}
+
+function OnClickMoney(amount, cost) {
+    if (cost <= multiplier) {
+        rebirth = rebirth + amount;
+        document.getElementById("rebirthCount").innerHTML = rebirth;
         document.getElementById("multiplierCount").innerHTML = multiplier;
-        document.getElementById("clickCount").innerHTML = counter;
+        document.getElementById("moneyCount").innerHTML = money;
     }
 }
