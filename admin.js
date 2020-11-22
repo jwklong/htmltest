d6d041efd6d37d228743f0d8e162190ce105751e
Alpha-404 <63340001+Alpha-404@users.noreply.github.com>
2020-11-22 10:32:18 +0000

    Create admin.js

---
+++ admin.js
@@ -0,0 +1,15 @@
+function dev() {
+    var inf = 999999999
+    alert('Loading GodMode');
+    money = inf;
+    multiplier = inf;
+    rebirth = inf;
+    ultrarebirth = inf;
+    prestige = inf;
+    OnClickMoney(1, 0, 0)
+    OnClickMultiplier(1,0,0)
+    OnClickRebirth(1,0,0)
+    OnClickUltra(1,0,0)
+    OnClickPrestige(1,0)
+    alert('GodMode Activated')
+}
